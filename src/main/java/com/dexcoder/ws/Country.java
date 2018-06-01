f1642217ce58e93e54eca6f966a746195b3a610a
liaojinhong <377890166@qq.com>
2018-06-01 15:55:53 +0800

    dut to git-spring-boot is imported to eclipse,so some files change

--- src/main/java/com/dexcoder/ws/Country.java
+++ src/main/java/com/dexcoder/ws/Country.java
@@ -2,7 +2,7 @@
 // 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.7 生成的
 // 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2018.05.30 时间 02:36:28 PM CST 
+// 生成时间: 2018.06.01 时间 02:19:37 PM CST 
 //
 
 
